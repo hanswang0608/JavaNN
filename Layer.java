9a3a64513b97f467289de38f5297a24f248ccbf3
Hans Wang <hanswang0168@gmail.com>
2023-10-28 01:50:23 -0400

    add getters/setters to Layer class

--- Layer.java
+++ Layer.java
@@ -3,15 +3,20 @@ import java.util.ArrayList;
 public class Layer {
     private ArrayList<Double> biases;
     private ArrayList<ArrayList<Double>> weights;
+    private ArrayList<Double> values;
 
-    public Layer(int numNeurons, int numWeights) {
-        this.biases = new ArrayList<Double>(numNeurons);
-        this.weights = new ArrayList<ArrayList<Double>>(numNeurons);
+    public Layer(int size, int numWeights) {
+        this.biases = new ArrayList<Double>(size);
+        this.values= new ArrayList<Double>(size);
+        this.weights = new ArrayList<ArrayList<Double>>(size);
 
-        for (int i = 0; i < numNeurons; i++) {
+        for (int i = 0; i < size; i++) {
             this.biases.add(i, 0.0);
         }
-        for (int i = 0; i < numNeurons; i++) {
+        for (int i = 0; i < size; i++) {
+            this.values.add(i, 0.0);
+        }
+        for (int i = 0; i < size; i++) {
             this.weights.add(i, new ArrayList<Double>(numWeights));
             for (int j = 0; j < numWeights; j++) {
                 this.weights.get(i).add(j, 0.0);
@@ -19,14 +24,18 @@ public class Layer {
         }
     }
 
-    public Layer(int numNeurons, int numWeights, double[] biases, double[][] weights) {
-        this.biases = new ArrayList<Double>(numNeurons);
-        this.weights = new ArrayList<ArrayList<Double>>(numNeurons);
+    public Layer(int size, int numWeights, double[] biases, double[][] weights) {
+        this.biases = new ArrayList<Double>(size);
+        this.values= new ArrayList<Double>(size);
+        this.weights = new ArrayList<ArrayList<Double>>(size);
 
-        for (int i = 0; i < numNeurons; i++) {
+        for (int i = 0; i < size; i++) {
             this.biases.add(i, biases[i]);
         }
-        for (int i = 0; i < numNeurons; i++) {
+        for (int i = 0; i < size; i++) {
+            this.values.add(i, 0.0);
+        }
+        for (int i = 0; i < size; i++) {
             this.weights.add(i, new ArrayList<Double>(numWeights));
             for (int j = 0; j < numWeights; j++) {
                 this.weights.get(i).add(j, weights[i][j]);
@@ -38,15 +47,69 @@ public class Layer {
         return this.biases.size();
     }
 
-    public Double[] getBiases() {
-        return this.biases.toArray(Double[]::new);
+    public int getNumWeights() {
+        return this.weights.get(0).size();
     }
 
-    public Double[][] getWeights() {
-        Double[][] ret = new Double[this.biases.size()][this.weights.get(0).size()];
-        for (int i = 0; i < this.biases.size(); i++) {
-            ret[i] = this.weights.get(i).toArray(Double[]::new);
+    public double[] getBiases() {
+        int numNeurons = getSize();
+        double[] ret = new double[numNeurons];
+        for (int i = 0; i < numNeurons; i++) {
+            ret[i] = this.biases.get(i).doubleValue();
         }
         return ret;
+    }
+
+    public void setBiases(double[] biases) {
+        int numNeurons = getSize();
+        if (biases.length != numNeurons) {
+            throw new IllegalArgumentException("Size of arg is different than biases");
+        }
+        for (int i = 0; i < numNeurons; i++) {
+            this.biases.set(i, biases[i]);
+        }
+    }
+
+    public double[] getValues() {
+        int numNeurons = getSize();
+        double[] ret = new double[numNeurons];
+        for (int i = 0; i < numNeurons; i++) {
+            ret[i] = this.values.get(i).doubleValue();
+        }
+        return ret;
+    }
+
+    public double[][] getWeights() {
+        int numNeurons = getSize();
+        int numWeights = getNumWeights();
+        double[][] ret = new double[numNeurons][numWeights];
+        for (int i = 0; i < numNeurons; i++) {
+            for (int j = 0; j < numWeights; j++) {
+                ret[i][j] = this.weights.get(i).get(j).doubleValue();
+            }
+        }
+        return ret;
+    }
+
+    public void setWeights(double[][] weights) {
+        int numNeurons = getSize();
+        int numWeights = getNumWeights();
+        if (weights.length != numNeurons || weights[0].length != numWeights) {
+            throw new IllegalArgumentException("Size of arg is different than weights");
+        }
+        for (int i = 0; i < numNeurons; i++) {
+            for (int j = 0; j < numWeights; j++) {
+                this.weights.get(i).set(j, weights[i][j]);
+            }
+        }
+    }
+
+    public void printValues() {
+        int numNeurons = getSize();
+        String s = "";
+        for (int i = 0; i < numNeurons; i++) {
+            s += this.values.get(i).toString() + ' ';
+        }
+        System.out.println(s);
     }
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        Layer l = new Layer(2, 2);
-        Double[] b = l.getBiases();
+        double[] b = new double[]{1.0, 2.0};
+        double[][] w = new double[2][];
+        w[0] = new double[]{3.0, 4.0}; 
+        w[1] = new double[]{5.0, 6.0}; 
+        Layer l = new Layer(2, 2, b, w);
+        b = l.getBiases();
         for (int i = 0; i < b.length; i++) {
             System.out.println(b[i]);
         }
-        Double[][] w = l.getWeights();
+        w = l.getWeights();
         for (int i = 0; i < w.length; i++) {
             for (int j = 0; j < w[i].length; j++) {
                 System.out.println(w[i][j]);
             }
         }
+        b = new double[]{1.1, 2.2};
+        l.setBiases(b);
+        b = l.getBiases();
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+        w[0] = new double[]{3.3, 4.4}; 
+        w[1] = new double[]{5.5, 6.6}; 
+        l.setWeights(w);
+        w = l.getWeights();
+        for (int i = 0; i < w.length; i++) {
+            for (int j = 0; j < w[i].length; j++) {
+                System.out.println(w[i][j]);
+            }
+        }
+        l.printValues();
     }
 }
