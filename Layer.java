8688488e67a669dcc680cb6599a7e5768b3ef3ea
Hans Wang <hanswang0168@gmail.com>
2023-10-31 20:37:28 -0400

    population crossover/mutate

--- Layer.java
+++ Layer.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Layer {
     private ArrayList<Double> biases;
--- Population.java
+++ Population.java
@@ -13,6 +13,30 @@ public class Population {
     public void evolve() {
 
     }
+    
+    public static void crossover(Agent a1, Agent a2) {
+        Chromosome c1 = a1.getChromosome();
+        Chromosome c2 = a2.getChromosome();
+        Chromosome.crossover(c1, c2);
+    }
 
+    public static void mutate(Agent agent) {
+        Chromosome chromosome = agent.getChromosome();
+        chromosome.mutate();
+    }
+
+    public static void main(String[] args) {
+        int[] arch = new int[]{2,2,2};
+        Agent a1 = new Agent(arch);
+        Agent a2 = new Agent(arch);
+        a1.getChromosome().printChromosome();
+        a2.getChromosome().printChromosome();
+
+        Population.mutate(a1);
+        Population.mutate(a2);
+        
+        a1.getChromosome().printChromosome();
+        a2.getChromosome().printChromosome();
+    }
 
 }
