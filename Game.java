bf1223674749337eaeaced1aa30ec3eaa0ee10f4
Hans Wang <hanswang0168@gmail.com>
2023-11-01 22:58:56 -0400

    can iterate on population but doesn't seem to converge towards higher fitness for dummy fitness function

--- Game.java
+++ Game.java
@@ -7,9 +7,34 @@ public class Game {
         this.population = new Population(populationSize, networkArchitecture);
     }
 
+    public void act(double[] inputs) {
+        for (Agent agent : population.getAgents()) {
+            agent.act(inputs);
+            agent.updateFitness();
+        }
+    }
+
+    public void evolve() {
+        population.selectParentsByRank();
+        population.crossoverPopulation();
+        population.mutatePopulation();
+    }
+
+    public void start() {
+        for (int i = 0; i < numIterations; i++) {
+            double[] inputs = {1,1};
+            act(inputs);
+            population.sortAgentsByFitness();
+            population.printAgents(true);
+            if (i < numIterations - 1) {
+                evolve();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Game game = new Game(2, 2, new int[]{2,2,2});
-        
+        Game game = new Game(100, 10, new int[]{2,2,2});
+        game.start();
     }
 
 }
